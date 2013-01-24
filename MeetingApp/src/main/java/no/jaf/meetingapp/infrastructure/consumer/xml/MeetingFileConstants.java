4ca44a13ad36dade52a0499893b8e227b1d47c7c
Jon Fagerholm <jon.fagerholm@gmail.com>
2013-01-24 15:58:40 +0100

    Jobber med gitignore

---
+++ MeetingApp/src/main/java/no/jaf/meetingapp/infrastructure/consumer/xml/MeetingFileConstants.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.jaf.meetingapp.infrastructure.consumer.xml;
+
+/**
+ *
+ * @author Jon
+ */
+public class MeetingFileConstants {
+
+    public static final String NODE_DATE = "dato";
+    public static final String NODE_TITLE = "tittel";
+    public static final String NODE_HOST = "arrangor";
+    public static final String NODE_BG_COLOR = "bakgrunnsfarge";
+    public static final String NODE_TIME_START = "tidStart";
+    public static final String NODE_TIME_END = "tidSlutt";
+    public static final String NODE_BY = "av";
+    public static final String NODE_SUBJECT = "emne";
+    public static final String NODE_SLOT = "presentasjon";
+}
